d26d0ba89ce4412b99adac26191931f935e38ca3
Miguel Saliba <36637899+miguelsaliba@users.noreply.github.com>
2024-03-14 19:19:52 -0400

    init

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#define GLEW_STATIC 1   // This allows linking with Static Library on Windows, without DLL
+
+// IMPORTANT 1
+#include <GL/glew.h>    // Include GLEW - OpenGL Extension Wrangler
+#include <GLFW/glfw3.h> // GLFW provides a cross-platform interface for creating a graphical context,
+                        // initializing OpenGL and binding inputs
+
+#include <glm/glm.hpp>  // GLM is an optimized math library with syntax to similar to OpenGL Shading Language
+
+int main (int argc, char *argv[]) {
+
+    return 0;
+}
