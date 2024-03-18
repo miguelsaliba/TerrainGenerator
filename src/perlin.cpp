784eb32c1b922f3f0f5606b5482fab1f48f9fa0e
Miguel Saliba <36637899+miguelsaliba@users.noreply.github.com>
2024-03-18 16:10:27 -0400

    added perlin functions

---
+++ src/perlin.cpp
@@ -0,0 +1,54 @@
+#include "perlin.h"
+#include <algorithm>
+#include <cmath>
+#include <random>
+
+Perlin::Perlin() : Perlin(0) {}
+
+Perlin::Perlin(unsigned int seed) {
+    for (int i = 0; i < 256; i++) {
+        p.push_back(i);
+    }
+
+    // shuffle using the given seed
+    std::shuffle(p.begin(), p.end(), std::default_random_engine(seed));
+
+    // Duplicate the permutation vector
+    p.insert(p.end(), p.begin(), p.end());
+}
+
+float Perlin::noise(float x, float y) {
+    int X = (int) std::floor(x) & 255;
+    int Y = (int) std::floor(y) & 255;
+    int g1 = p[p[X] + Y];
+    int g2 = p[p[X + 1] + Y];
+    int g3 = p[p[X] + Y + 1];
+    int g4 = p[p[X + 1] + Y + 1];
+
+    x -= std::floor(x);
+    y -= std::floor(y);
+
+    float u = fade(x);
+    float v = fade(y);
+
+    float x1Interp = lerp(u, grad(g1, x, y), grad(g2, x - 1, y));
+    float x2Interp = lerp(u, grad(g3, x, y - 1), grad(g4, x - 1, y - 1));
+
+    return lerp(v, x1Interp, x2Interp);
+}
+
+float Perlin::lerp(float amount, float left, float right) {
+    return left + amount * (right - left);
+}
+
+float Perlin::fade(float t) {
+    return t * t * t * (t * (t * 6 - 15) + 10);
+}
+
+float Perlin::grad(int hash, float x, float y) {
+    int h = hash & 7;
+    float u = h < 4 ? x : y;
+    float v = h < 4 ? y : x;
+    return ((h & 1) ? -u : u) + ((h & 2) ? -2.0f * v : 2.0f * v);
+}
+
---
+++ src/perlin.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <vector>
+
+// 2D version of Ken Perlin's noise algorithm: https://mrl.cs.nyu.edu/~perlin/noise/
+
+class Perlin {
+public:
+	Perlin();
+	Perlin(unsigned int seed);
+	float noise(float x, float y);
+	float octaveNoise(float x, float y, int octaves, float persistence);	
+private:
+	static float lerp(float amount, float left, float right);
+	static float grad(int hash, float x, float y);
+	static float fade(float t);
+	std::vector<int> p;
+};
+
